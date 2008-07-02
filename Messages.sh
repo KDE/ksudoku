ca8c98414c09e77eaefdc67e28655f0cb38584dd
Laurent Montel <montel@kde.org>
2008-07-02 06:49:42 +0000

    Remove temporary files
    svn path=/trunk/KDE/kdegames/ksudoku/; revision=827038

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 $EXTRACTRC $(find src/ -name "*.rc") >> rc.cpp || exit 11
 $EXTRACTRC $(find src/ -name "*.ui") >> rc.cpp || exit 12
 $XGETTEXT $(find src/ -name "*.cpp") rc.cpp -o $podir/ksudoku.pot
-
+rm -f rc.cpp
