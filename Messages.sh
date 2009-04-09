e395c803e946f183572656c6fdf97aa461c9f5f3
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:33:09 +0000

    Updated Messages.sh files to find strings from .kcfg files.
    svn path=/trunk/KDE/kdegames/ksudoku/; revision=951690

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 $EXTRACTRC $(find src/ -name "*.rc") >> rc.cpp || exit 11
 $EXTRACTRC $(find src/ -name "*.ui") >> rc.cpp || exit 12
+$EXTRACTRC $(find src/ -name "*.kcfg") >> rc.cpp
 $XGETTEXT $(find src/ -name "*.cpp") rc.cpp -o $podir/ksudoku.pot
 rm -f rc.cpp
