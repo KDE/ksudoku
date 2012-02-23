1b20fee61e278d860b88dd1fe37e60a101feac54
Ian Wadham <iandw.au@gmail.com>
2012-02-23 04:14:22 +0000

    BUG: 294420 FIXED-IN: 4.8.1 Make the value-list widget non-empty at creation time, so that KSudoku will not crash when there is a resize event before starting the first puzzle.
    svn path=/trunk/KDE/kdegames/ksudoku/; revision=1281829

--- src/gui/views/valuelistwidget.cpp
+++ src/gui/views/valuelistwidget.cpp
@@ -126,7 +126,7 @@ ValueListWidget::ValueListWidget(QWidget* parent)
 	m_selectionItem = new SelectionItem();
 	m_scene->addItem(m_selectionItem);
 	
-	setMaxValue(1);
+	setMaxValue(1);	// Make SymbolItem list non-empty, in case of a resize.
 	m_selectedValue = 1;
 }
 
