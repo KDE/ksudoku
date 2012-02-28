6b914bf3e21ee7b106c6da85335c0f38263f5d69
Allen Winter <winter@kde.org>
2012-02-28 14:02:43 +0000

    In deserializeClique(), make sure to return something (return true if all goes well). fixes compile for me else
    CCMAIL: iandw.au@gmail.com
    
    
    svn path=/trunk/KDE/kdegames/ksudoku/; revision=1282644

--- src/gui/serializer.cpp
+++ src/gui/serializer.cpp
@@ -50,9 +50,9 @@ Game Serializer::deserializeGame(QDomElement element) {
 	Puzzle* puzzle = 0;
 	bool hasHistory = false;
 	QList<HistoryEvent> history;
-	
+
 	bool hadHelp = static_cast<bool>(element.attribute("had-help", "0").toInt());
-	
+
 	QDomNode child = element.firstChild();
 	while (!child.isNull()) {
 		if(child.isElement()) {
@@ -61,7 +61,7 @@ Game Serializer::deserializeGame(QDomElement element) {
 					delete puzzle;
 					return Game();
 				}
-					
+
 				puzzle = deserializePuzzle(child.toElement());
 				hasPuzzle = true;
 			} else if(child.nodeName() == "history") {
@@ -69,24 +69,24 @@ Game Serializer::deserializeGame(QDomElement element) {
 					delete puzzle;
 					return Game();
 				}
-				
+
 				history = deserializeHistory(child.toElement());
 				hasHistory = true;
 			}
 		}
 		child = child.nextSibling();
 	}
-	
+
 	if(!puzzle) return Game();
-	
+
 	Game game(puzzle);
 	game.setUserHadHelp(hadHelp);
-	
+
 	if(hasHistory) {
 		for(int i = 0; i < history.count(); ++i)
 			game.doEvent(history[i]);
 	}
-	
+
 	return game;
 }
 
@@ -97,7 +97,7 @@ Puzzle* Serializer::deserializePuzzle(QDomElement element) {
 	QString valuesStr;
 	bool hasSolution = false;
 	QString solutionStr;
-	
+
 	QString content;
 	QDomNode child = element.firstChild();
 	while (!child.isNull()) {
@@ -107,7 +107,7 @@ Puzzle* Serializer::deserializePuzzle(QDomElement element) {
 					delete graph;
 					return 0;
 				}
-					
+
 				graph = deserializeGraph(child.toElement());
 				hasGraph = true;
 			} else if(child.nodeName() == "values") {
@@ -115,7 +115,7 @@ Puzzle* Serializer::deserializePuzzle(QDomElement element) {
 					delete graph;
 					return 0;
 				}
-				
+
 				valuesStr = child.toElement().text();
 				hasValues = true;
 			} else if(child.nodeName() == "solution") {
@@ -124,14 +124,14 @@ Puzzle* Serializer::deserializePuzzle(QDomElement element) {
 					delete graph;
 					return 0;
 				}
-				
+
 				solutionStr = child.toElement().text();
 				hasSolution = true;
 			}
 		}
 		child = child.nextSibling();
 	}
-	
+
 	if(!graph) return 0;
 	if(valuesStr.length() != graph->size()) {
 		delete graph;
@@ -142,15 +142,15 @@ Puzzle* Serializer::deserializePuzzle(QDomElement element) {
 		delete graph;
 		return 0;
 	}
-	
+
 	Puzzle* puzzle = new Puzzle(graph, hasSolution);
-	
+
 	BoardContents values;
 	values.resize(graph->size());
 	for(int i = 0; i < graph->size(); ++i) {
 		values[i] = Symbols::ioSymbol2Value(valuesStr[i]);
 	}
-	
+
 	// TODO remove deserialization of solution, it is no longer required
 	BoardContents solution;
 	if(solutionStr.length() != 0) {
@@ -159,7 +159,7 @@ Puzzle* Serializer::deserializePuzzle(QDomElement element) {
 			solution[i] = Symbols::ioSymbol2Value(solutionStr[i]);
 		}
 	}
-	
+
 	puzzle->init(values);
 	return puzzle;
 }
@@ -180,18 +180,18 @@ static int readInt(QDomElement element, const QString& name, int* err)
 
 SKGraph* Serializer::deserializeGraph(QDomElement element) {
 	bool noFailure = true;
-	
+
 	QString orderStr = element.attribute("order");
 	if(orderStr.isNull())
 		return 0;
 	int order = orderStr.toInt(&noFailure, 0);
 	if(!noFailure)
 		return 0;
-	
+
 	QString type = element.attribute("type");
 	if(type.isNull())
 		return 0;
-	
+
 	bool d3 = false;
 	if(type == "sudoku") {
 		SKGraph *graph = new SKGraph(order, TypeSudoku);
@@ -281,11 +281,12 @@ bool Serializer::deserializeClique(SKGraph * graph, const QString & size,
 	}
     }
     graph->addClique(data);
+    return true;
 }
 
 QList<HistoryEvent> Serializer::deserializeHistory(QDomElement element) {
 	QList<HistoryEvent> history;
-	
+
 	QDomNode child = element.firstChild();
 	while (!child.isNull()) {
 		if(child.isElement()) {
@@ -306,21 +307,21 @@ HistoryEvent Serializer::deserializeSimpleHistoryEvent(QDomElement element) {
 	QString valueStr = element.attribute("value");
 	bool given = element.attribute("given") == "true";
 	bool noFailure = true;
-	
+
 	int index = indexStr.toInt(&noFailure, 0);
 	if(!noFailure)
 		return HistoryEvent();
-	
-	
+
+
 	if(markerStr.isNull() == valueStr.isNull())
 		return HistoryEvent();
-	
-	
+
+
 	if(!markerStr.isNull()) {
 		QBitArray markers(markerStr.length());
 		for(int i = 0; i < markerStr.length(); ++i)
 			markers[i] = markerStr[i] != '0';
-		
+
 		return HistoryEvent(index, CellInfo(markers));
 	} else {
 		int value = valueStr.toInt(&noFailure, 0);
@@ -333,7 +334,7 @@ HistoryEvent Serializer::deserializeSimpleHistoryEvent(QDomElement element) {
 			return HistoryEvent(index, CellInfo(CorrectValue, value));
 		}
 	}
-	
+
 	return HistoryEvent();
 }
 
@@ -366,7 +367,7 @@ SKGraph *Serializer::loadCustomShape(const KUrl &url, QWidget* window, QString *
 			*errorMsg = i18n("Cannot load file.");
 		return 0;
 	}
-	
+
 	QDomNode child = doc.documentElement().firstChild();
 	while (!child.isNull()) {
 		if(child.isElement()) {
@@ -376,7 +377,7 @@ SKGraph *Serializer::loadCustomShape(const KUrl &url, QWidget* window, QString *
 		}
 		child = child.nextSibling();
 	}
-	
+
 	return 0;
 }
 
@@ -403,18 +404,18 @@ Game Serializer::load(const KUrl& url, QWidget* window, QString *errorMsg) {
 			*errorMsg = i18n("Cannot load file.");
 		return Game();
 	}
-	
+
 	// used to ensure, that there is only one game
 	bool hasGame = false;
 	Game game;
-	
+
 	QDomNode child = doc.documentElement().firstChild();
 	while (!child.isNull()) {
 		if(child.isElement()) {
 			if(child.nodeName() == "game") {
 				if(hasGame)
 					return Game();
-				
+
 				game = deserializeGame(child.toElement());
 				hasGame = true;
 			}
@@ -436,19 +437,19 @@ bool Serializer::serializeGame(QDomElement& parent, const Game& game) {
 
 bool Serializer::serializePuzzle(QDomElement& parent, const Puzzle* puzzle) {
 	QString contentStr;
-	
+
 	QDomDocument doc = parent.ownerDocument();
 	QDomElement element = doc.createElement("puzzle");
 	serializeGraph(element, puzzle->graph());
-	
+
 	for(int i = 0; i < puzzle->size(); ++i) {
 		contentStr += Symbols::ioValue2Symbol(puzzle->value(i));
 	}
-	
+
 	QDomElement content = doc.createElement("values");
 	content.appendChild(doc.createTextNode(contentStr));
 	element.appendChild(content);
-	
+
 	if(puzzle->hasSolution()) {
 		contentStr = QString();
 		for(int i = 0; i < puzzle->size(); ++i) {
@@ -458,7 +459,7 @@ bool Serializer::serializePuzzle(QDomElement& parent, const Puzzle* puzzle) {
 		content.appendChild(doc.createTextNode(contentStr));
 		element.appendChild(content);
 	}
-	
+
 	parent.appendChild(element);
 	return true;
 }
@@ -467,7 +468,7 @@ bool Serializer::serializeGraph(QDomElement &parent, const SKGraph *graph)
 {
 	QDomElement element = parent.ownerDocument().createElement("graph");
 	element.setAttribute("order", graph->order());
-	
+
 	GameType type = graph->type();
 	element.setAttribute("type" , (type == TypeSudoku) ? "sudoku" :
 				(type == TypeRoxdoku) ? "roxdoku" : "custom");
@@ -477,7 +478,7 @@ bool Serializer::serializeGraph(QDomElement &parent, const SKGraph *graph)
 	for (n = 0; n < EndSudokuTypes; n++) {
 	    if (puzzleType == types [n]) {
 		break;
-	    }	
+	    }
 	}
 	element.setAttribute("specific-type", (n < 0) ? "Plain" : typeNames[n]);
 
@@ -510,27 +511,27 @@ bool Serializer::serializeGraph(QDomElement &parent, const SKGraph *graph)
 
 bool Serializer::serializeHistory(QDomElement& parent, const Game& game) {
 	QDomElement element = parent.ownerDocument().createElement("history");
-	
+
 	for(int i = 0; i < game.historyLength(); ++i) {
 		if(!serializeHistoryEvent(element, game.historyEvent(i)))
 			return false;
 	}
-	
+
 	parent.appendChild(element);
 	return true;
 }
 
 bool Serializer::serializeHistoryEvent(QDomElement& parent, const HistoryEvent& event) {
 	QDomElement element;
-	 
+
 	const QVector<int>& indices = event.cellIndices();
 	const QVector<CellInfo>& changes = event.cellChanges();
-	
+
 	if(indices.count() == 0) {
 		return true;
 	} else if(indices.count() == 1) {
 		element = parent.ownerDocument().createElement("simple-event");
-		
+
 		element.setAttribute("index", indices[0]);
 		switch(changes[0].state()) {
 			case GivenValue:
@@ -544,12 +545,12 @@ bool Serializer::serializeHistoryEvent(QDomElement& parent, const HistoryEvent& 
 				break;
 			case Marker: {
 				QString str;
-				
+
 				QBitArray markers = changes[0].markers();
 				for(int j = 0; j < markers.size(); ++j) {
 					str += markers[j] ? '1' : '0';
 				}
-				
+
 				element.setAttribute("markers", str);
 			} break;
 		}
@@ -557,7 +558,7 @@ bool Serializer::serializeHistoryEvent(QDomElement& parent, const HistoryEvent& 
 		element = parent.ownerDocument().createElement("complex-event");
 		for(int i = 0; i < indices.count(); ++i) {
 			QDomElement subElement = parent.ownerDocument().createElement("simple-event");
-			
+
 			subElement.setAttribute("index", indices[i]);
 			switch(changes[i].state()) {
 				case GivenValue:
@@ -571,20 +572,20 @@ bool Serializer::serializeHistoryEvent(QDomElement& parent, const HistoryEvent& 
 					break;
 				case Marker: {
 					QString str;
-					
+
 					QBitArray markers = changes[i].markers();
 					for(int j = 0; j < markers.size(); ++j) {
 						str += markers[i] ? '1' : '0';
 					}
-					
+
 					subElement.setAttribute("markers", str);
 				} break;
 			}
-			
+
 			element.appendChild(subElement);
 		}
 	}
-	
+
 	parent.appendChild(element);
 	return true;
 }
@@ -592,17 +593,17 @@ bool Serializer::serializeHistoryEvent(QDomElement& parent, const HistoryEvent& 
 bool Serializer::store(const Game& game, const KUrl& url, QWidget* window) {
 	QDomDocument doc( "ksudoku" );
 	QDomElement root = doc.createElement( "ksudoku" );
-	doc.appendChild( root );	
-	
+	doc.appendChild( root );
+
 	serializeGame(root, game);
-	
+
 	KTemporaryFile file;
 	file.open();
-	
+
 	QTextStream stream(&file);
 	stream << doc.toString();
 	stream.flush();
-	
+
 	KIO::NetAccess::upload(file.fileName(), url, window);
 	return true;
 }
