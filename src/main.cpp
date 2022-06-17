91554644de1d8f05c89ec3a25924a1fd9c055b9e
Friedrich W. H. Kossebau <kossebau@kde.org>
2022-06-17 23:36:34 +0200

    Update homepage to latest apps.kde.org URL
    GIT_SILENT

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
 	                 i18n("KSudoku - Sudokus and more"),
 	                 KAboutLicense::GPL_V2,
 	                 i18n("(c) 2005-2007 The KSudoku Authors"),
-	                 QString(), QStringLiteral("https://games.kde.org/games/ksudoku"));
+	                 QString(), QStringLiteral("https://apps.kde.org/ksudoku"));
 	about.addAuthor( i18n("Francesco Rossi"), i18n("KSudoku Author"), QStringLiteral("redsh@email.it") );
 	about.addAuthor( i18n("Johannes Bergmeier"), i18n("Maintainer"), QStringLiteral("Johannes.Bergmeier@gmx.net") );
 	about.addAuthor( i18n("Ian Wadham"), i18n("New puzzle generator and solver"), QStringLiteral("iandw.au@gmail.com") );
